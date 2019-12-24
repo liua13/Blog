cddf30439e8a9fff0d2f945ef983e5604f31fed9
Annie Liu <liua1@bxscience.edu>
2019-12-24 12:17:40 -0500

    added css and js

---
+++ assets/JS/index.js
@@ -0,0 +1,53 @@
+greetings = ["student.", "coder.", "writer."];
+wordLength = 0;
+index = 0;
+
+changeScroll = 1;
+previousScroll = 0;
+
+$(document).ready(function(){
+	message = $("#greetings");
+	typing();
+	setInterval(function(){
+		cursor();
+	}, 600);
+});
+
+function typing(){
+	if(index >= greetings.length){
+		index = 0;
+	}
+	
+	message.text(greetings[index].substr(0, wordLength++));
+	if(wordLength <= greetings[index].length){
+		setTimeout(function(){
+			typing(greetings[index]);
+		}, 50);
+	} else {
+		setTimeout(function(){
+			erasing();
+		}, 500);
+	}
+}
+
+function erasing(){
+	message.text(greetings[index].substr(0, wordLength--));
+	if(wordLength >= 0){
+		setTimeout(function(){
+			erasing();
+		}, 50);
+	} else {
+		setTimeout(function(){
+			index++;
+			typing();
+		}, 500);
+	}
+}
+
+function cursor(){
+	$("#cursor").animate({
+		opacity: 0
+	}, 300, "swing").animate({
+		opacity: 1
+	}, 300, "swing")
+}
