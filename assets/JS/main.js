64b686b0f9c4f26104e1b7501c9af5f30a076374
Annie Liu <liua1@bxscience.edu>
2019-12-25 18:05:19 -0500

    added about me section

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -4,15 +4,12 @@ $(window).scroll(function() {
 	console.log(y);
 	console.log(y);
 	if (y < 180) {
-		$("#nav-title").css("display", "none");
 		$("#nav").css("display", "inline-block");
 		$("#nav").css("background-color", "transparent");
 	} else if(y < 690) {
-		$("#nav-title").css("display", "none");
 		$("#nav").css("display", "inline-block");
 		$("#nav").css("background-color", "#469DD6");
 	} else {
-		$("#nav-title").css("display", "inline");
 	    if(previousY > y){
 	    	$("#nav").fadeIn();
 	    } else {
