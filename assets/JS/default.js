971f5099dfafcabdef948acf1175cf7f97a59ff2
liua13 <liua1@bxscience.edu>
2020-06-08 17:09:53 -0400

    changed favicon, nav babar

--- assets/JS/default.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() {
-	var y = $(this).scrollTop();
-
-	if(y < 75){
-    	$("#nav").show();
-    } else {
-    	$("#nav").hide();
-    }
-});
--- assets/JS/home.js
+++ assets/JS/home.js
@@ -75,3 +75,17 @@ $(window).scroll(function() {
 	previousY = y;
 });
 */
+
+$(window).scroll(function() {
+	var y = $(this).scrollTop();
+
+	if(y < 50){
+    	$("#nav").removeClass('nav-scroll');
+    	$(".page-link").css('color', 'white');
+    	$(".page-link:after").css('background-color', 'white');
+    } else {
+    	$("#nav").addClass('nav-scroll');
+    	$(".page-link").css('color', '#09283a');
+    	$(".page-link:after").css('background-color', '#09283a');
+    }
+});
