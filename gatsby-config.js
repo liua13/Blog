ca98a08341f7c883c5f3e62a4eab44865064a037
liua13 <liua1@bxscience.edu>
2020-07-31 20:40:01 -0400

    added google analytics tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,13 @@ module.exports = {
         name: `posts`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "235498460",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
